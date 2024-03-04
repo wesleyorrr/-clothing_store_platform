86a694afa3fe79317f346e389280d5796f8db089
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-03-04 18:18:11 -0300

    ClothingStorePlatform - modifier LoadingDialog

--- app/src/main/java/com/example/clothingstoreplatform/util/components/LoadingDialog.kt
+++ app/src/main/java/com/example/clothingstoreplatform/util/components/LoadingDialog.kt
@@ -1,8 +1,22 @@
 package com.example.clothingstoreplatform.util.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 
 @Composable
-fun LoadingDialog() {
+fun LoadingDialog(isLoading: Boolean) {
+    if (isLoading){
+        Dialog(onDismissRequest = { /*TODO*/ },
+            properties = DialogProperties(dismissOnClickOutside = false)
+            ) {
+            Box(modifier = Modifier.width(200.dp))
+
+        }
+    }
 }
