770259e0b0de375d5efe96fddbbf58f6d55e4504
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-03-05 17:55:27 -0300

    ClothingStorePlatform - modifier LoadingDialog - modifier MyTopAppBar - modifier ProductsScreen.kt

--- app/src/main/java/com/example/clothingstoreplatform/store/presentation/products_screen/ProductsScreen.kt
+++ app/src/main/java/com/example/clothingstoreplatform/store/presentation/products_screen/ProductsScreen.kt
@@ -1,6 +1,17 @@
 package com.example.clothingstoreplatform.store.presentation.products_screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.clothingstoreplatform.util.components.LoadingDialog
+import com.example.clothingstoreplatform.util.components.MyTopAppBar
 
 @Composable
 internal fun ProductScreen(){
@@ -10,4 +21,24 @@ internal fun ProductScreen(){
 @Composable
 fun ProductsContent(
     state: ProductViewState
-){}
+){
+    LoadingDialog(isLoading = state.isLoading)
+    Scaffold (modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MyTopAppBar(title  = "Products")
+
+
+        }
+    ){
+        LazyVerticalStaggeredGrid(
+            modifier = Modifier.padding(top = it.calculateTopPadding()),
+            columns = StaggeredGridCells.Fixed(2) ,
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            verticalItemSpacing = 10.dp
+
+        ){
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/clothingstoreplatform/util/components/LoadingDialog.kt
+++ app/src/main/java/com/example/clothingstoreplatform/util/components/LoadingDialog.kt
@@ -1,9 +1,16 @@
 package com.example.clothingstoreplatform.util.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -15,7 +22,16 @@ fun LoadingDialog(isLoading: Boolean) {
         Dialog(onDismissRequest = { /*TODO*/ },
             properties = DialogProperties(dismissOnClickOutside = false)
             ) {
-            Box(modifier = Modifier.width(200.dp))
+            Box(modifier = Modifier
+                .width(200.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.White),
+                contentAlignment = Alignment.Center
+
+
+            ){
+                CircularProgressIndicator(modifier = Modifier.padding(10.dp))
+            }
 
         }
     }
