a7ef1b1424459b985228a4533b1759d1d0ace159
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-03-08 17:21:59 -0300

    ClothingStorePlatform -alt ProductsViewModel.kt

--- app/src/main/java/com/example/clothingstoreplatform/store/presentation/products_screen/componets/ProductsViewModel.kt
+++ app/src/main/java/com/example/clothingstoreplatform/store/presentation/products_screen/componets/ProductsViewModel.kt
@@ -1,15 +1,31 @@
 package com.example.clothingstoreplatform.store.presentation.products_screen.componets
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.clothingstoreplatform.store.domain.repository.ProductsRepository
+import com.example.clothingstoreplatform.store.presentation.products_screen.ProductViewState
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductsViewModel @Inject constructor(
     private val productsRepository: ProductsRepository
 ):ViewModel() {
+   private val _state = MutableStateFlow(ProductViewState())
+    val state = _state.asStateFlow()
+
+    fun getProducts(){
+        viewModelScope.launch {  }
+        _state.update {
+            it.copy(isLoading = true)
+        }
+
+    }
 
 
 }
