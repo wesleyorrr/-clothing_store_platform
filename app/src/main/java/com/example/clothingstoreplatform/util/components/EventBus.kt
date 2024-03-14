03ad87c054ee6a6bbee19e009e37354c0d08933b
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-03-14 16:03:02 -0300

    ClothingStorePlatform - create EventBus

---
+++ app/src/main/java/com/example/clothingstoreplatform/util/components/EventBus.kt
@@ -0,0 +1,18 @@
+package com.example.clothingstoreplatform.util.components
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+
+object EventBus {
+    private val _events = Channel<Any>()
+    val events = _events.receiveAsFlow()
+
+    suspend fun  sendEvent(event:Any){
+        _events.send(event)
+
+    }
+}
+
+sealed interface Event{
+    data class Toast (val menssage: String): Event
+}
