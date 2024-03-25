12a7b359366ac4d9e608ded08d8edf24f45e679c
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-03-25 19:34:07 -0300

    ClothingStorePlatform
    - DELETE

--- app/src/main/java/com/example/clothingstoreplatform/store/data/remote/ProductApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.clothingstoreplatform.store.data.remote
-
-import com.example.clothingstoreplatform.store.domain.model.Product
-import retrofit2.http.GET
-
-interface ProductApi {
-@GET("Products")
-suspend fun getProducts():List<Product>
-}
